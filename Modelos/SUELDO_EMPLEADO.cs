266bc93cf1cb0d49b7e5c566b3fe4c5e315a575d
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-02 13:45:34 -0600

    Se agrega modelos de las tablas direccion y sueldo

---
+++ Modelos/SUELDO_EMPLEADO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class SUELDO_EMPLEADO
+    {
+        private string iD;
+        private int dPI;
+        private decimal sUELDO_BASE;
+        private decimal bONIFICACION;
+
+        public string ID { get => iD; set => iD = value; }
+        public int DPI { get => dPI; set => dPI = value; }
+        public decimal SUELDO_BASE { get => sUELDO_BASE; set => sUELDO_BASE = value; }
+        public decimal BONIFICACION { get => bONIFICACION; set => bONIFICACION = value; }
+    }
+}
