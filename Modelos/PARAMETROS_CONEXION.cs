e10db160c71d22f6aff53786f922eff06e50a21a
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-02 14:05:13 -0600

    Se agrega modelo para obtener los datos de conexion

---
+++ Modelos/PARAMETROS_CONEXION.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class PARAMETROS_CONEXION
+    {
+        private string dB;
+        private string sERVER;
+        private string uSER;
+        private string pSW;
+
+        public string DB { get => dB; set => dB = value; }
+        public string SERVER { get => sERVER; set => sERVER = value; }
+        public string USER { get => uSER; set => uSER = value; }
+        public string PSW { get => pSW; set => pSW = value; }
+    }
+}
