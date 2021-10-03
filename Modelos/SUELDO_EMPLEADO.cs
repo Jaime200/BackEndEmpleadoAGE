46d129e96da9092276bb42352278cebc4427a9e5
Jaime Muñoz <jaime.munoz200@gmail.com>
2021-10-02 19:17:35 -0600

    Se modifica tipo de dato en ID

--- Modelos/SUELDO_EMPLEADO.cs
+++ Modelos/SUELDO_EMPLEADO.cs
@@ -8,12 +8,12 @@ namespace Modelos
 {
     public class SUELDO_EMPLEADO
     {
-        private string iD;
+        private int iD;
         private int dPI;
         private decimal sUELDO_BASE;
         private decimal bONIFICACION;
 
-        public string ID { get => iD; set => iD = value; }
+        public int ID { get => iD; set => iD = value; }
         public int DPI { get => dPI; set => dPI = value; }
         public decimal SUELDO_BASE { get => sUELDO_BASE; set => sUELDO_BASE = value; }
         public decimal BONIFICACION { get => bONIFICACION; set => bONIFICACION = value; }
